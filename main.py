81d6fe933c939da447e113be66c253434fb365d1
Ravi Kiran <rkiran030190@gmail.com>
2025-11-23 10:29:55 +0530

    added routers, models and database conn

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from bnfweb!")
+from fastapi import FastAPI
+import uvicorn
+from routes import (
+    theatre_routes,
+    slots_routes
+)
 
+app = FastAPI(title="Event Booking API", version="1.0")
+
+# Register routers
+app.include_router(theatre_routes.router)
+app.include_router(slots_routes.router)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to BNF"}
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ src/core/slots_routes.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException
+import csv
+from io import StringIO
+from config.db_config import MongoDBHandler
+from datetime import datetime
+
+router = APIRouter(prefix="/api/slots", tags=["Slots"])
+
+# Initialize DB handler
+db_handler = MongoDBHandler(db_name="BNF", collection_name="Slot")
+db_handler.connect()
+
+@router.post("/upload_csv")
+async def upload_slot_csv(file: UploadFile = File(...)):
+    """
+    Ingest theatres data from a CSV file into MongoDB.
+    Expected CSV headers: ID, Name, Location, PricePerPerson
+    """
+    try:
+        content = await file.read()
+        decoded = content.decode("utf-8")
+        reader = csv.DictReader(StringIO(decoded))
+
+        slots = []
+        for row in reader:
+            slot = {
+                "Name": row["Name"],
+                "End": datetime.strptime(row["End"],"%d-%m-%Y %H:%M"),
+                "Start": datetime.strptime(row["Start (Date & Time)"], "%d-%m-%Y %H:%M"),
+                "Coupon" : row["Applied coupon (YES/NO)"],
+                "Theatre" : row["Theatre ID"],
+                "Price" : row["price"]
+            }
+            slots.append(slot)
+
+        if slots:
+            inserted_ids = db_handler.insert_many(slots)
+            return {"message": f"Inserted {len(inserted_ids)} slots successfully"}
+        else:
+            raise HTTPException(status_code=400, detail="CSV file is empty or invalid")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing file: {str(e)}")
