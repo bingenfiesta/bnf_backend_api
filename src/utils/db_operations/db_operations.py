8685803aff57e187888a08afc04abd5668450c14
Blackskis-code <blackskis.rk@gmail.com>
2025-12-04 22:22:56 +0530

    updated code

--- src/utils/db_operations/db_operations.py
+++ src/utils/db_operations/db_operations.py
@@ -4,6 +4,11 @@ from os import getenv
 
 class BnFMongoManager:
 
-    def __init__(self):
+    def __init__(self, collection_name: str):
         self.__client = MongoClient(getenv("MONGO_CONNECTION_STRING", ""))
+        self.__db = self.__client[getenv("MONGO_DB_NAME", "BNF")]
+        self.collection_name = collection_name
+
+    def add_records(self):
+        self.collection = self.__db[self.collection_name]
         
